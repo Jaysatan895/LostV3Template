9ab7cf51202631873f352881458f4636aeae0b6e
Jayson Atanasoff-McGuire <jaysatan895@ed.amdsb.ca>
2022-03-28 14:23:32 -0400

    fixed

--- LostAdventure/Form1.cs
+++ LostAdventure/Form1.cs
@@ -172,12 +172,12 @@ namespace LostAdventure
                     imageBox.Image = Properties.Resources.banker;
 
 
-                    break;
-                case 8:
-                    outputLabel.Text = "";
-                    option1Label.Text = "Yes";
-                    option2Label.Text = "No";
-                    break;
+             //       break;
+            //    case 8:
+             //       outputLabel.Text = "";
+             //       option1Label.Text = "Yes";
+              //      option2Label.Text = "No";
+              //      break;
 
                 case 9:
                     outputLabel.Text = "Walk home or walk to the bank?";
